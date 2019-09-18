b82198e631cf102d34febda9a1b6a5940ede3af2
EugeneTepin <4godwork@gmail.com>
2019-09-18 14:02:26 +0300

    Added nav-link-click event using ref

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import React from "react";
 
 export class NavLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.aRef = React.createRef();
+  }
+  componentDidMount() {
+    this.aRef.current.addEventListener("click", e => {
+      var event = new CustomEvent("nav-link-click", {
+        detail: this.props,
+        bubbles: true
+      });
+      this.aRef.current.dispatchEvent(event);
+      e.preventDefault();
+    });
+  }
+
   render() {
     const {
       to,
@@ -13,6 +28,9 @@ export class NavLink extends React.Component {
       "aria-current": aria_current,
       ...other
     } = this.props;
+
+    other.href = to;
+    other.ref = this.aRef;
     return React.createElement("a", other, " ", children);
   }
 }
