d1bd998819b7a5d0ccc2f0b679db2f8ad11676d3
EugeneTepin <4godwork@gmail.com>
2019-09-18 14:37:37 +0300

    Use react click handler to dispatch custom event.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 export class NavLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.aRef = React.createRef();
+  }
+
   render() {
     const {
       to,
@@ -10,9 +15,26 @@ export class NavLink extends React.Component {
       exact,
       strict,
       location,
+      onClick,
       "aria-current": aria_current,
       ...other
     } = this.props;
+
+    other.href = to;
+    other.ref = this.aRef;
+    
+    other.onClick = (e) => {
+      var event = new CustomEvent("nav-link-click", {
+        detail: this.props,
+        bubbles: true
+      });
+      this.aRef.current.dispatchEvent(event);
+      if(onClick){
+        onClick(e);
+      }
+      e.preventDefault();
+    };
     return React.createElement("a", other, " ", children);
   }
 }
+
